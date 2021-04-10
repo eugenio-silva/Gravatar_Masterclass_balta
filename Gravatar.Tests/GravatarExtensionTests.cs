a9a4c1a639c9e43e95388b02aabe30e000ce07a7
Lucas Silva <lucasmedja1@gmail.com>
2021-04-09 22:02:27 -0300

    Iniciando o Projeto

---
+++ Gravatar.Tests/GravatarExtensionTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Gravatar.Tests
+{
+    [TestClass]
+    public class GravatarExtensionTests 
+    {
+        [TestMethod]
+        public void shouldValidateGravatar()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ Gravatar/GravatarExtension.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Gravatar
+{
+    public static class GravatarExtension
+    {
+        public static string ToGravatar(this string email) {
+            if(string.IsNullOrEmpty(email))
+            return string.Empty;
+
+            using var md5 = MD5.Create();
+            var inputBytes = Encoding.ASCII.GetBytes(email);
+            var hashBytes = md5.ComputeHash(inputBytes);
+            
+            var sb = new StringBuilder();
+            foreach (var t in hashBytes)
+                sb.Append(t.ToString("X2"));
+
+            return $"https://www.gravatar.com/avatar/{sb.ToString().ToLower()}";
+        }
+    }
+}
